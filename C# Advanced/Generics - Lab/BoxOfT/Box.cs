a9752fbd04f06bbdb9564375236b65e37fc8b989
Ivan Ganchev <djonatan2002@gmail.com>
2024-12-25 12:08:45 +0200

    Added Generics-Lab

---
+++ C# Advanced/Generics - Lab/BoxOfT/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> list = new Stack<T>();
+        public int Count
+        {
+            get { return this.list.Count; }
+        }
+
+        public void Add(T element)
+        {
+                this.list.Push(element);
+            
+        }
+
+        public T Remove()
+        {
+            return this.list.Pop();
+        }
+    }
+}
---
+++ C# Advanced/Generics - Lab/GenericScale/EqualityScale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericScale
+{
+    internal class EqualityScale<T>
+    {
+        public EqualityScale(T left, T right)
+        {
+            this._left = left;
+            this._right = right;
+        }
+
+        public T _left;
+        public T _right;
+
+        public bool AreEqual()
+        {
+            return _left.Equals(_right);
+        }
+    }
+}
