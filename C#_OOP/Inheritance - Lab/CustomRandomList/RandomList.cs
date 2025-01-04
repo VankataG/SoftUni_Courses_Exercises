28419603294b2225a561ae73d699aa37cb6bc3b8
Ivan Ganchev <djonatan2002@gmail.com>
2025-01-04 23:17:03 +0200

    Added Inheritance Lab

---
+++ C#_OOP/Inheritance - Lab/CustomRandomList/RandomList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(0, this.Count);
+            string result = this[index];
+            this.Remove(this[index]);
+            return result;
+        }
+    }
+}
---
+++ C#_OOP/Inheritance - Lab/CustomStack/StackOfStrings.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Count == 0)
+                return true;
+            return false;
+        }
+
+        public void AddRange(IEnumerable<string> elements)
+        {
+            foreach (var element in elements)
+            {
+                this.Push(element);
+            }
+        }
+    }
+}
